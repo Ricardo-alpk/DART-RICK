369e0b1f8fb23a76797043d67218cd6ae3cecc6b
ricar <ricardo.man.ramirez@gmail.com>
2026-05-06 21:32:17 +0200

    Hice un ejercici mas

--- ejercicio3.dart
+++ ejercicio3.dart
@@ -3,7 +3,19 @@ import 'dart:io';
 
 void main(){
 
-  dynamic name = 'hola mundo';
+  final DateTime hoy = DateTime.now();
+
+  print('AÑO ACTUAL ${hoy.year}');
+  print('MES ACTUAL ${hoy.month}');
+  print('DIA ACTUAL ${hoy.day}');
+
+  //Probando con las horas
+
+  print('Hora ${hoy.hour}');
+  print('Minutos ${hoy.minute}');
+  print('Segundos ${hoy.second}');
+
+  print(hoy);
 
 
 
