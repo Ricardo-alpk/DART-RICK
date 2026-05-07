cca28c8b8be4a3b3b786c76f6252b77bcff36153
ricar <ricardo.man.ramirez@gmail.com>
2026-05-07 20:04:37 +0200

    Hice un ejercicio mas

--- ejercicio4.dart
+++ ejercicio4.dart
@@ -1,15 +1,14 @@
 import 'dart:io';
 
-void main(){
+void main() {
   const double pi = 3.14159;
 
   print('INTRODUCE EL VALOR DEL LARIO DEL CIRCULO');
   var radio = stdin.readLineSync();
-  
+
   int numero = int.parse(radio!);
 
   double area = pi * numero * numero;
 
   print('EL AREA DEL CIRCULO EL ${area}');
-
 }
