7a57535f941eac0e854bc1469ea2399a830930da
ricar <ricardo.man.ramirez@gmail.com>
2026-05-07 20:04:29 +0200

    Hice un ejercicio mas

--- ejercicio5.dart
+++ ejercicio5.dart
@@ -1,19 +1,38 @@
 import 'dart:io';
 
 void main(){
+  /**
+   * Lee dos números enteros desde consola.
+
+Muestra la suma, resta, producto, división real, división entera y el resto en líneas diferentes.
+
+Si la entrada no es válida, muestra un mensaje de “Error” y finaliza el programa.
+   */
   print('INTRODUCE 2 NUMEROS ENTEROS');
 
-  var num1 = stdin.readLineSync();
+  //var num1 = stdin.readLineSync(); normalmente pondriamos esto
+  //pero se que stdin recoge strings asi que ya lo ponemos asi directamente
+  String? num1 = stdin.readLineSync();
 
-  var num2 = stdin.readLineSync();
+  String? num2 = stdin.readLineSync();
   
-  int num1int = int.parse(num1!);
+  int? n1 = int.tryParse(num1 ?? '');
 
-  int num2int = int.parse(num2!);
+  int? n2 = int.tryParse(num2 ?? '');
 
-  print('SUMA: ${num1int + num2int}');
-  print('RESTA: ${num1int - num2int}');
-  print('DIVISION REAL ${num1int / num2int}');
+  if (n1 == null || n2 == null){
+    print('ERROR');
+    return;
+  }
+
+  
+
+  print('SUMA: ${n1 + n2}');
+  print('RESTA: ${n1 - n2}');
+  print('DIVISION REAL ${n1 / n2}');
+  print('DIVISION ENTERA ${n1 ~/ n2}');
+
+
 
 
 }
