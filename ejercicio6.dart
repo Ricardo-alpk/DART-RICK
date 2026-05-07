eb36c3419bf09cb197aae460af74f39a46f63d31
ricar <ricardo.man.ramirez@gmail.com>
2026-05-07 20:42:49 +0200

    Hice un ejercicio mas

--- ejercicio6.dart
+++ ejercicio6.dart
@@ -1,5 +1,23 @@
 import 'dart:io';
 
 void main(){
+/**
+ * Lee un número decimal (`double`) desde consola.
+
+Imprime el valor original y el mismo número redondeado a dos decimales.
+
+Muestra también el tipo de cada variable para comprobar la inferencia.
+ */
+
+  String? num = stdin.readLineSync();
+
+  //No olvidar que devolvera null si ve que la entrada no es un numero valido
+  double? numd = double.tryParse(num ?? '');
+
+  double redondeado = double.parse(numd!.toStringAsFixed(2));
+
+  print('ORIGINAL : $numd');
+  print('Redondeado : ${redondeado}');
+  
 
 }
